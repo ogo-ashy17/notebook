c36db88295546362e606f42ea0845bec90182cde
Xiang <winding.f.f.roak@gmail.com>
2024-12-01 23:02:37 +0900

    flask env

--- introduction-to-falsk/apps/minimalapp/app.py
+++ introduction-to-falsk/apps/minimalapp/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Flaskbook!"
+    return "Hello, Flaskbook! degbug mode on ファイルを保存して再読み込みしないと画面に反映されない。せめて保存したら自動で読みんで欲しいものだが。"
