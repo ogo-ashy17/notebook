0b405bfc94ab880dc259869dcffeee95bb8dea29
Xiang <winding.f.f.roak@gmail.com>
2024-12-02 22:05:55 +0900

    routinig

--- introduction-to-falsk/apps/minimalapp/app.py
+++ introduction-to-falsk/apps/minimalapp/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, Flaskbook! degbug mode on ファイルを保存して再読み込みしないと画面に反映されない。せめて保存したら自動で読みんで欲しいものだが。"
+
+# ルーティング：リクエスト先のURIと関数を紐づける
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
