649f0621386ea7505e74cc7bb151f768a6d7724d
Xiang <winding.f.f.road@gmail.com>
2024-12-07 19:14:55 +0900

    flask

--- introduction-to-flask/apps/minimalapp/app.py
+++ introduction-to-flask/apps/minimalapp/app.py
@@ -1,6 +1,43 @@
-from flask import Flask, render_template, url_for, request, redirect, current_app, g
+import logging
+import os
+from email_validator import validate_email, EmailNotValidError
+from flask import (
+    Flask, 
+    render_template, 
+    url_for, 
+    request, 
+    redirect, 
+    current_app, 
+    g, 
+    flash
+)
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+# SECRET_KEYを設定する
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+# ログレベルを設定する
+app.logger.setLevel(logging.DEBUG)
+# リダイレクトを中断しないようにする
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# DEbugToolbaExtensionにアプリケーションをセットする
+toolbar = DebugToolbarExtension(app)
+# Mailクラスのコンフィグを追加する
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+# flask-mail拡張機能を登録する
+mail = Mail(app)
+
+app.logger.critical("fatal error")
+app.logger.error("error")
+app.logger.warning("warning")
+app.logger.info("info")
+app.logger.debug("debug")
 
 @app.route("/")
 def index():
@@ -34,9 +71,54 @@ def contact():
 @app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
-        # メールを送る（最後に実装）
+        # form属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
 
+        # 入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+        # メールを送る
+        send_email(
+            email,
+            "問い合わせありがとうございました。",
+            "contact_mail",
+            username=username,
+            description=description,
+        )
+
+        # 問い合わせ完了エンドポイントへリダイレクト
+        flash("問い合わせ内容はメールにて送信しました。問い合わせありがとうございます。")
         # contactエンドポイントへリダイレクトする
         return redirect(url_for("contact_complete"))
     
     return render_template("contact_complete.html")
+
+def send_email(to, subject, template, **kwargs):
+    """メールを送信する関数"""
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
